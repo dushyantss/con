49a5556fb11131e57d91e3137ad360b31f654a2b
Dushyant Singh Shekhawat <contact@dushyantss.com>
2022-02-14 01:29:46 +0530

    Enable file concatenation

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,36 @@
-use std::io::{self, BufRead};
+use std::{
+    env,
+    error::Error,
+    fs::File,
+    io::{self, StdoutLock},
+};
 
-fn main() {
-    for line in io::stdin().lock().lines() {
-        match line {
-            Err(_) => break,
-            Ok(s) => println!("{}", s),
-        }
+fn main() -> Result<(), Box<dyn Error>> {
+    let out_handler = io::stdout();
+    let out_handler = out_handler.lock();
+
+    let args: Vec<_> = env::args().into_iter().skip(1).collect();
+
+    if args.is_empty() {
+        pipe_stdin_out(out_handler)?;
+    } else {
+        print_all_files(out_handler, args)?;
     }
+
+    Ok(())
+}
+
+fn pipe_stdin_out(mut out_handler: StdoutLock) -> Result<(), Box<dyn Error + 'static>> {
+    let in_handler = io::stdin();
+    let mut in_handler = in_handler.lock();
+    io::copy(&mut in_handler, &mut out_handler)?;
+    Ok(())
+}
+
+fn print_all_files(mut out_handler: StdoutLock, args: Vec<String>) -> Result<(), Box<dyn Error>> {
+    for filename in &args {
+        let mut reader = File::open(filename)?;
+        io::copy(&mut reader, &mut out_handler)?;
+    }
+    Ok(())
 }
